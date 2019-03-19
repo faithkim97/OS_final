e139a682d7f0c8775084e862654286b0d1c6e4c4
hafsah786 <hhanif@smith.edu>
2019-03-19 14:33:44 -0400

    commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Application started");
+    }
+}
+
+// CodeCracker class
+// while loop: timer > 0: game is playing
+// 
---
+++ src/main/java/CodeCracker.java
@@ -0,0 +1,6 @@
+// This checks the characters in the input against the assigned password
+public class CodeCracker {
+    private int inputPassword;
+    private int setPassword;
+
+}
